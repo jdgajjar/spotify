e54121e2d1ab713d3b83200f8b196945fcb9c64e
Your Name <your_email@example.com>
2025-02-23 14:15:30 +0530

    update for deploy

--- scr/config/deployconfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    // Example configuration
-    publicPath: process.env.NODE_ENV === 'production' ? '/spotify/' : '/',
-    outputDir: 'dist',
-    assetsDir: 'static',
-    devServer: {
-      proxy: 'https://your-api-endpoint.com'
-    }
-  };
--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+
 
 
 const playButton = document.getElementById("play");
